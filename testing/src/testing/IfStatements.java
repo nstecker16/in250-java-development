31ddfc8a6e1b882611ec9f5d1602c990987c7cc6
nstecker16 <nstecker16@gmail.com>
2025-08-25 14:39:49 -0500

    creating IfStatements test class, testing for u1

---
+++ testing/src/testing/IfStatements.java
@@ -0,0 +1,17 @@
+// Testing out if statements
+
+package testing;
+
+public class IfStatements {
+	
+	public static void main(String[] args) {
+		// Declaring a string
+		String myString = "True";
+		
+		// Conditional if statement to print if True
+		if (myString == "True") {
+			System.out.println("myString is True");
+		}
+		
+	}
+}
