b4be2652e4c615eef27b6c226c5a5de9fbb96740
nstecker16 <nstecker16@gmail.com>
2025-08-25 11:47:56 -0500

    adding a new project called testing

---
+++ testing/src/testing/ConvertingToInt.java
@@ -0,0 +1,17 @@
+package testing;
+
+public class ConvertingToInt {
+	
+	public static void main(String[] args) {
+		
+		// Declaring a string
+		String myString = "100";
+		
+		// Converting myString to an integer
+		int myInt = myString;
+		
+		// Displaying myInt
+		System.out.println(myInt);
+		
+	}
+}
