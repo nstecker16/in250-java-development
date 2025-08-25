be07bd827d338059d7a211d5e275d46519157159
nstecker16 <nstecker16@gmail.com>
2025-08-25 15:29:13 -0500

    adding Math.java to test math operations

---
+++ testing/src/testing/Math.java
@@ -0,0 +1,21 @@
+// Testing out math operations
+
+package testing;
+
+public class Math {
+
+	public static void main(String[] args) {
+
+		// Declaring some variables
+		double baseDouble = 100;
+		final double myMultiplier = 1.1;
+		double adjustedMyDouble = baseDouble;
+
+		// Testing some operations
+		adjustedMyDouble *= myMultiplier;
+
+		// Displaying results
+		System.out.println(adjustedMyDouble);
+	}
+
+}
