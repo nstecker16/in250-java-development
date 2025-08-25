2db321d51cd4d84f8d4e7b312798c75669858132
nstecker16 <nstecker16@gmail.com>
2025-08-24 22:29:11 -0500

    declaring strings and constant values

--- Unit1Assignment/src/org/nathan/in252/unit1/IN252_U1M1.java
+++ Unit1Assignment/src/org/nathan/in252/unit1/IN252_U1M1.java
@@ -1,10 +1,27 @@
+// Code for Unit 1 Assignment
+
 package org.nathan.in252.unit1;
 
 public class IN252_U1M1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Declaring strings outlining student information
+		String name = "John Smith";
+		String address = "101 N. Main Street";
+		String city = "anytown";
+		String state = "Texas";
+		String zip = "11111";
+		String unitsTaken = "19";
+		String prevPerformance = "Good";
+		
+		// Declaring constant variables for price and AI parameters
+		final double pricePerUnit = 100.50;
+		final double baseDiscount = 150;
+		final double aiPerformanceMultiplier = 1.1;
+		
+		// 
 	}
 
 }
