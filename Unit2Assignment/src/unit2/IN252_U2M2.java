e100771e14350a3414091a1b2963802388cb7ff1
nstecker16 <nstecker16@gmail.com>
2025-08-29 14:39:12 -0500

    initializing Unit 2 assignment project

---
+++ Unit2Assignment/src/unit2/IN252_U2M2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+package unit2;
+
+public class IN252_U2M2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		/* Section 1: Sentiment Analysis Comparisons
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Sentiment analysis determines emotional tone or attitude
+		 * outputted by AI in text data via assignment of numerical
+		 * scores
+		 */ 
+
+		System.out.println(\nSection 1: Sentiment Analysis Comparisons");
+
+
+	}
+
+}
